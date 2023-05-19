5a374d90a6a9b233def6c5875c56ac499e7f7caa
MountainBikerYYC <ywang9696@gmail.com>
2023-05-18 22:47:44 -0600

    Created mongodb connection

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
 const app = express();
+
+//Connect to database
+connectDB();
 
 app.use(
   "/graphql",
