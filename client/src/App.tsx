d485b212128819796f0e1bca873b02d524e30c91
MountainBikerYYC <ywang9696@gmail.com>
2023-05-22 22:10:03 -0600

    Got delete graphql working, updates cache

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,28 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
 const uri: string = "http://localhost:5000/graphql";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: uri,
-  cache: new InMemoryCache(),
+  cache: cache,
 });
 function App() {
   return (
---
+++ client/src/components/AddClientModal.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+
+const AddClientModal = () => {
+  return <div>AddClientModal</div>;
+};
+
+export default AddClientModal;
