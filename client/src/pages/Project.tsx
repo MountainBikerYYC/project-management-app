d5d6314b0e50be5dbca7815746431cda82d2ee4d
MountainBikerYYC <ywang9696@gmail.com>
2023-05-23 19:17:27 -0600

    WIP on project page

--- client/src/pages/Project.tsx
+++ client/src/pages/Project.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Link, useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECT } from "../queries/ProjectQueries";
 
 const Project = () => {
-  return (
-    <div>Project</div>
-  )
-}
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT);
+  return <div>{id}</div>;
+};
 
-export default Project
+export default Project;
