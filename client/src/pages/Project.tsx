315730b3967516f0352a02a820d7578ef40cc7ad
MountainBikerYYC <ywang9696@gmail.com>
2023-05-24 23:18:35 -0600

    WIP project page

--- client/src/pages/Project.tsx
+++ client/src/pages/Project.tsx
@@ -5,8 +5,18 @@ import { GET_PROJECT } from "../queries/ProjectQueries";
 
 const Project = () => {
   const { id } = useParams();
-  const { loading, error, data } = useQuery(GET_PROJECT);
-  return <div>{id}</div>;
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Something Went Wrong</p>;
+  return <>
+    {!loading && !error && (
+        <div className="mx-auto w-75 card p-5">
+            <Link to="/" className="btn btn-light btn-sm "></Link>
+
+        </div>
+    )}
+  </>
 };
 
 export default Project;
